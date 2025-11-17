55b496dc24b6ca416e6de4e2222b9bb1ec26bb55
makej7403-bit <makej7403@gmail.com>
2025-11-17 15:53:33 +0000

    Create page.js

---
+++ app/categories/page.js
@@ -0,0 +1,8 @@
+export default function CategoriesPage() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Categories</h1>
+      <p>Select any category to explore news.</p>
+    </main>
+  );
+}
