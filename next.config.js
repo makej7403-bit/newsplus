36e4659319fc0fff4ca7572e1b94a4ba73ceced0
makej7403-bit <makej7403@gmail.com>
2025-11-17 16:25:10 +0000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,18 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  env: {
-    NEWSDATA_API_KEY: process.env.NEWSDATA_API_KEY,
-    FIREBASE_API_KEY: process.env.FIREBASE_API_KEY
-  },
   images: {
-    domains: [
-      "cdn.thenewsapi.com",
-      "newsdata.io",
-      "i.ytimg.com",
-      "images.unsplash.com"
-    ]
-  }
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
