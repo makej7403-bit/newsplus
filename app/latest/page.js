240c2ab07e30b6916867e6d42f4890c037e247c7
makej7403-bit <makej7403@gmail.com>
2025-11-17 15:52:56 +0000

    Create page.js

---
+++ app/latest/page.js
@@ -0,0 +1,8 @@
+export default function LatestPage() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Latest News</h1>
+      <p>Live updates from around the world...</p>
+    </main>
+  );
+}
