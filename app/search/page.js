1a051758c7efbf341c0d1cd3a653933d40a221cf
makej7403-bit <makej7403@gmail.com>
2025-11-17 15:54:07 +0000

    Create page.js

---
+++ app/search/page.js
@@ -0,0 +1,8 @@
+export default function SearchPage() {
+  return (
+    <main style={{ padding: "20px" }}>
+      <h1>Search News</h1>
+      <p>Type keywords to find relevant news articles.</p>
+    </main>
+  );
+}
